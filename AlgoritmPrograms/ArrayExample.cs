738103fbf2e3e98ea3ea44ce9bec6e928f466d9c
Pc <Pc@DESKTOP-PRAMDG2>
2023-05-27 17:29:45 +0330

    fix

--- AlgoritmPrograms/ArrayExample.cs
+++ AlgoritmPrograms/ArrayExample.cs
@@ -61,8 +61,6 @@ namespace AlgoritmPrograms
                     {
                         t++;
                         k++;
-
-
                     }
                     else
                     {
@@ -75,6 +73,36 @@ namespace AlgoritmPrograms
 
             }
 
+
+        }
+        //if (i<arr1.Length)
+        //{
+        //    i++;
+        //}
+        //else
+        //{
+
+        //}
+
+        public void DifrenceBetweanTwoList()
+        {
+            int[] arr1 = new[] { 3, 2, 5, 6, 7 };
+            int[] arr2 = new[] { 3, 3, 4, 5, 7, 9 };
+            int s = 0;
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                for (int j = 0; j < arr2.Length; j++)
+                {
+                    if (j<arr2.Length)
+                    {
+                        j++;
+                    }
+                    else
+                    {
+                            
+                    }
+                }
+            }
 
         }
 

f828c9524b227d899b033ec0130d50b90b39f37d
Pc <Pc@DESKTOP-PRAMDG2>
2023-05-30 18:05:45 +0330

    fix

--- AlgoritmPrograms/8aueen.cs
+++ AlgoritmPrograms/8aueen.cs
@@ -34,6 +34,24 @@ namespace AlgoritmPrograms
         /// Finally, the PrintSolution method prints the board representation, using "Q" for queens and "." for empty spaces.
         /// 
         /// You can run the program, and it will print all possible solutions for the 8-queens problem. Each solution represents a valid arrangement of 8 queens on an 8x8 chessboard, where no two queens threaten each other.
+        ///
+        ///
+        /// .................................................................
+        /// Define the chessboard size (8x8 in this case).
+        /// Create an array to store the column position of each queen for each row.
+        /// Start with the first row (row 0).
+        /// For each column in the current row:
+        /// Check if it is safe to place a queen at the current position.
+        /// If it is safe, store the column position in the array and move to the next row.
+        /// If it is not safe, try the next column.
+        /// If all columns have been tried and none are safe, backtrack to the previous row.
+        /// If the current row is the last row (row 7 in the case of an 8x8 board), a solution has been found.
+        /// Print the solution or store it in a collection of solutions.
+        /// Backtrack to the previous row to find other solutions (if any).
+        /// Repeat steps 4-5 until all solutions have been found.
+        ///
+        ///
+        /// 
         /// </summary>
 
 
